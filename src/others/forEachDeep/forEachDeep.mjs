1aa7eb92e1a598009359c5121b2faf893b834ff7
yonghzhang <yonghzhang@tencent.com>
2025-09-09 10:44:13 +0800

    feat: 新增递归遍历 json 函数

---
+++ src/others/forEachDeep/forEachDeep.mjs
@@ -0,0 +1,35 @@
+/**
+ * 树形 json 的深度遍历
+ *
+ * @param {object | Array} obj 树形 json
+ * @param {(v: any, d: number, i: number)=> void} callback 遍历回调，v 项，d 深度，i 当前深度时的索引
+ * @param {object} [opts] 可控制参数
+ * @param {string} [opts.children='children'] 子节点 key
+ * @param {number} [opts.depth=Number.POSITIVE_INFINITY] 可遍历深度
+ * @param {'child' | 'parent'} [opts.firstly="parent"] DPS 深度优先 BFS 广度优先
+ */
+export function forEachDeep(obj, callback, opts) {
+  // 入参校验，直接跳过
+  if (!obj) return
+  if (typeof callback !== 'function') return
+
+  obj = Array.isArray(obj) ? obj : [obj]
+  const {
+    children = 'children',
+    depth = Number.POSITIVE_INFINITY,
+    firstly = 'parent',
+  } = opts || {}
+
+  const traverse = (items, d = 0) => {
+    let i = 0
+    items.forEach((v) => {
+      if (firstly === 'parent') callback(v, d, i++)
+      if (children in v && d < depth) {
+        traverse(v[children], d + 1)
+      }
+      if (firstly === 'child') callback(v, d, i++)
+    })
+  }
+
+  traverse(obj)
+}
---
+++ src/others/forEachDeep/forEachDeep.spec.mjs
@@ -0,0 +1,77 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { forEachDeep } from './forEachDeep.mjs'
+
+describe('forEachDeep', () => {
+  beforeEach(() => vi.useFakeTimers())
+  afterEach(() => vi.useRealTimers())
+
+  const tree = [
+    { label: '全部', value: '', children: [
+      { label: '项目1', value: 'project1', children: [
+        { label: '子项目1', value: 'subProject1' },
+        { label: '子项目2', value: 'subProject2' },
+      ] },
+      { label: '项目2', value: 'project2' },
+    ] },
+  ]
+
+  it('normal case', async () => {
+    const callback = vi.fn()
+    forEachDeep(tree, callback)
+    expect(callback).toBeCalledTimes(5)
+    expect(callback).toHaveBeenNthCalledWith(1, tree[0], 0, 0)
+    expect(callback).toHaveBeenNthCalledWith(2, tree[0].children[0], 1, 0)
+    expect(callback).toHaveBeenNthCalledWith(3, tree[0].children[0].children[0], 2, 0)
+    expect(callback).toHaveBeenNthCalledWith(4, tree[0].children[0].children[1], 2, 1)
+    expect(callback).toHaveBeenNthCalledWith(5, tree[0].children[1], 1, 1)
+  })
+
+  it('children option', async () => {
+    const tree = [{ label: '全部', value: '', items: [{ label: '项目1', value: 'project1' }] }]
+    const callback = vi.fn()
+    forEachDeep(tree, callback, { children: 'items' })
+    expect(callback).toBeCalledTimes(2)
+    expect(callback).toHaveBeenNthCalledWith(1, tree[0], 0, 0)
+    expect(callback).toHaveBeenNthCalledWith(2, tree[0].items[0], 1, 0)
+  })
+
+  it('depth option', async () => {
+    const callback1 = vi.fn()
+    forEachDeep(tree, callback1, { depth: 0 })
+    expect(callback1).toBeCalledTimes(1)
+    expect(callback1).toHaveBeenNthCalledWith(1, tree[0], 0, 0)
+
+    const callback2 = vi.fn()
+    forEachDeep(tree, callback2, { depth: 1 })
+    expect(callback2).toBeCalledTimes(3)
+    expect(callback1).toHaveBeenNthCalledWith(1, tree[0], 0, 0)
+    expect(callback2).toHaveBeenNthCalledWith(2, tree[0].children[0], 1, 0)
+    expect(callback2).toHaveBeenNthCalledWith(3, tree[0].children[1], 1, 1)
+  })
+
+  it('firstly option', async () => {
+    const callback = vi.fn()
+    forEachDeep(tree, callback, { firstly: 'child' })
+    expect(callback).toBeCalledTimes(5)
+    expect(callback).toHaveBeenNthCalledWith(1, tree[0].children[0].children[0], 2, 0)
+    expect(callback).toHaveBeenNthCalledWith(2, tree[0].children[0].children[1], 2, 1)
+    expect(callback).toHaveBeenNthCalledWith(3, tree[0].children[0], 1, 0)
+    expect(callback).toHaveBeenNthCalledWith(4, tree[0].children[1], 1, 1)
+    expect(callback).toHaveBeenNthCalledWith(5, tree[0], 0, 0)
+  })
+
+  it('unexpected object argument', async () => {
+    const callback1 = vi.fn()
+    forEachDeep(undefined, callback1)
+    expect(callback1).toBeCalledTimes(0)
+
+    const callback2 = vi.fn()
+    forEachDeep(null, callback2)
+    expect(callback2).toBeCalledTimes(0)
+
+    const callback3 = vi.fn()
+    forEachDeep({ a: 1 }, callback3)
+    expect(callback3).toBeCalledTimes(1)
+    expect(callback3).toHaveBeenNthCalledWith(1, { a: 1 }, 0, 0)
+  })
+})
